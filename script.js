742f1f94c40ecb84e9a12099dd2d9123e791e1ed
rakesh <rakeshkumarsah333@gmail.com>
2021-10-24 13:00:38 +0530

    init

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 console.log("Welcome to Spotify");
+//console .log
 
 
 // Initialize the Variables
@@ -11,61 +12,60 @@ let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 //creating array 
 let songs = [
-    {songName: "Warriyo - Mortals [NCS Release]", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Cielo - Huma-Huma", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
-    {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
-    {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
-    {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
-    {songName: "Marlo Lighter", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
-    {songName: "Best of 2021 - DJNYK", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
-    {songName: "Dum Dee Dum - Keys N Krates", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
-    {songName: "Major Lazer - Nyk", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
-    {songName: "Need For speed - Tyko Drift", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
-     {songName: "Eminem - Monster", filePath: "songs/11.mp3", coverPath: "covers/11.jpg"},
-]
-//forEach take call back function
-songItems.forEach((element, i)=>{ 
-    element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
-    element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
+        { songName: "Warriyo - Mortals [NCS Release]", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
+        { songName: "Cielo - Huma-Huma", filePath: "songs/2.mp3", coverPath: "covers/2.jpg" },
+        { songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "songs/3.mp3", coverPath: "covers/3.jpg" },
+        { songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs/4.mp3", coverPath: "covers/4.jpg" },
+        { songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg" },
+        { songName: "Marlo Lighter", filePath: "songs/2.mp3", coverPath: "covers/6.jpg" },
+        { songName: "Best of 2021 - DJNYK", filePath: "songs/2.mp3", coverPath: "covers/7.jpg" },
+        { songName: "Dum Dee Dum - Keys N Krates", filePath: "songs/2.mp3", coverPath: "covers/8.jpg" },
+        { songName: "Major Lazer - Nyk", filePath: "songs/2.mp3", coverPath: "covers/9.jpg" },
+        { songName: "Need For speed - Tyko Drift", filePath: "songs/4.mp3", coverPath: "covers/10.jpg" },
+        { songName: "Eminem - Monster", filePath: "songs/11.mp3", coverPath: "covers/11.jpg" },
+    ]
+    //forEach take call back function
+songItems.forEach((element, i) => {
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
 })
- 
+
 
 // Handle play/pause click
-masterPlay.addEventListener('click', ()=>{
-    if(audioElement.paused || audioElement.currentTime<=0){
-        audioElement.play();
-        masterPlay.classList.remove('fa-play-circle');
-        masterPlay.classList.add('fa-pause-circle');
-        gif.style.opacity = 1;
-    }
-    else{
-        audioElement.pause();
-        masterPlay.classList.remove('fa-pause-circle');
-        masterPlay.classList.add('fa-play-circle');
-        gif.style.opacity = 0;
-    }
-})
-// Listen to Events
-audioElement.addEventListener('timeupdate', ()=>{ 
+masterPlay.addEventListener('click', () => {
+        if (audioElement.paused || audioElement.currentTime <= 0) {
+            audioElement.play();
+            masterPlay.classList.remove('fa-play-circle');
+            masterPlay.classList.add('fa-pause-circle');
+            gif.style.opacity = 1;
+        } else {
+            audioElement.pause();
+            masterPlay.classList.remove('fa-pause-circle');
+            masterPlay.classList.add('fa-play-circle');
+            gif.style.opacity = 0;
+        }
+    })
+    // Listen to Events
+audioElement.addEventListener('timeupdate', () => {
     // Update Seekbar 
     // initialize veriable progress in percentage(parseInt)
-    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
+    progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
     myProgressBar.value = progress;
 })
 
-myProgressBar.addEventListener('change', ()=>{
-    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+myProgressBar.addEventListener('change', () => {
+    audioElement.currentTime = myProgressBar.value * audioElement.duration / 100;
 })
 
-const makeAllPlays = ()=>{
-    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+const makeAllPlays = () => {
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
         element.classList.remove('fa-pause-circle');
         element.classList.add('fa-play-circle');
     })
 }
 
-Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
-    element.addEventListener('click', (e)=>{ 
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
+    element.addEventListener('click', (e) => {
         makeAllPlays();
         songIndex = parseInt(e.target.id);
         e.target.classList.remove('fa-play-circle');
@@ -78,13 +78,12 @@ Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
         masterPlay.classList.remove('fa-play-circle');
         masterPlay.classList.add('fa-pause-circle');
     })
-}) 
+})
 
-document.getElementById('next').addEventListener('click', ()=>{
-    if(songIndex>=100){
+document.getElementById('next').addEventListener('click', () => {
+    if (songIndex >= 100) {
         songIndex = 0
-    }
-    else{
+    } else {
         songIndex += 1;
     }
     audioElement.src = `songs/${songIndex+1}.mp3`;
@@ -96,11 +95,10 @@ document.getElementById('next').addEventListener('click', ()=>{
 
 })
 
-document.getElementById('previous').addEventListener('click', ()=>{
-    if(songIndex<=0){
+document.getElementById('previous').addEventListener('click', () => {
+    if (songIndex <= 0) {
         songIndex = 0
-    }
-    else{
+    } else {
         songIndex -= 1;
     }
     audioElement.src = `songs/${songIndex+1}.mp3`;
